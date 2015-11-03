b5ea047732ee71f6ccdf689fdd011e3f2777fb9b
kageyama <merveilles19980318@yahoo.co.jp>
2015-11-03 12:28:55 +0900

    add rails

---
+++ rails/start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd /data
+rails new $PROJECT
+cd $PROJECT
+sed -ri "s/^# gem 'therubyracer'(.*)/gem 'therubyracer', platforms: :ruby/g" Gemfile
+bundle install
+
+rails s -b 0.0.0.0 -e development
