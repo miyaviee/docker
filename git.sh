9f57ac75ec6ac0b38b69526cb97c710e02c2b3cd
miyaviee <merveilles19980318@yahoo.co.jp>
2015-12-07 15:28:29 +0000

    update

--- git.sh
+++ git.sh
@@ -12,10 +12,12 @@ yum -y install \
 
 yum clean all
 
-cd /tmp \
-&& curl -SLO https://github.com/git/git/archive/v2.6.3.zip \
-&& unzip v2.6.3.zip \
-&& rm -f v2.6.3.zip
+if [ ! -e /tmp/git-2.6.3 ]; then
+  cd /tmp \
+  && curl -SLO https://github.com/git/git/archive/v2.6.3.zip \
+  && unzip v2.6.3.zip \
+  && rm -f v2.6.3.zip
+fi
 
 cd /tmp/git-2.6.3 \
 && make configure \
--- test/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "$1" == "setup" ]; then
-  service httpd stop
-fi
-
-if [ "$1" == "start" ]; then
-  /usr/sbin/httpd -D FOREGROUND
-fi
