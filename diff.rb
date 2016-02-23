619584bbfd250fbc82c9db567f370ba2276d1a6a
m-kageyama <m-kageyama@active.co.jp>
2016-02-23 04:57:15 +0000

    diff update

--- diff.rb
+++ diff.rb
@@ -27,50 +27,61 @@ module Diff
       w1 = ''
       w2 = ''
 
+      d1 = ''
+      d2 = ''
       loop do
-        if word1.first != word2.first
-          if word1.size <= word2.size
-            word, word2 = check word1, word2
-            w2 += set_diff word
-          else
-            word, word1 = check word2, word1
-            w1 += set_diff word
-          end
-        end
-
-        if word1.empty?
+        if word1.empty? or word2.empty?
+          w1 += d1
+          w2 += d2
+          w1 += set_diff word1.join
           w2 += set_diff word2.join
           break
         end
 
-        if word2.empty?
-          w1 += set_diff word1.join
-          break
+        if word1.first == word2.first
+          w1 += d1
+          w2 += d2
+          d1 = ''
+          d2 = ''
+          w1 += word1.shift
+          w2 += word2.shift
+          next
         end
 
-        w1 += word1.shift
-        w2 += word2.shift
+        k1 = word1.find_index word2.first
+        k2 = word2.find_index word1.first
+
+        if k1.nil? and k2.nil?
+          d1 += set_diff word1.shift
+          d2 += set_diff word2.shift
+          next
+        end
+
+        if a.nil?
+          d2 += set_diff word2.shift
+          next
+        end
+
+        if b.nil?
+          d1 += set_diff word1.shift
+          next
+        end
+
+        if a == b
+          d1 += set_diff word1.shift
+          d2 += set_diff word2.shift
+        elsif a > b
+          b.times do |n|
+            d2 += set_diff word2.shift
+          end
+        else
+          a.times do |n|
+            d1 += set_diff word1.shift
+          end
+        end
       end
 
       return w1, w2
-    end
-
-    def check s, l
-      w = ''
-      loop do
-        break if l.first == s.first
-        k = l.find_index s.first
-        if k.nil?
-          w = l.join
-          l = []
-          break
-        end
-        k.times do
-          w += l.shift
-        end
-      end
-
-      return w, l
     end
 
   end
