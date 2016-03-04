77757d898cfa651b210d14190b3eec4923aa5279
m-kageyama <m-kageyama@active.co.jp>
2016-03-04 00:33:46 +0000

    update

--- capybara/spec/test.rb
+++ capybara/spec/test.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'capybara/rspec'
 require 'capybara/poltergeist'
+require 'fileutils'
 
 feature 'test' do
   before do
@@ -11,11 +12,15 @@ feature 'test' do
   after do |example|
     filename = "log/#{example.full_description}.png".gsub ' ', '/'
     dirname = File.dirname filename
-    Dir.mkdir dirname unless File.exist? dirname
+    FileUtils.mkdir_p dirname unless File.exist? dirname
     page.save_screenshot filename, :full => true
   end
 
-  scenario 'hokkaido' do
-    visit 'http://r.gnavi.co.jp/sp/s/kansougei/'
+  scenario '東京の少人数で個室' do
+    visit 'http://192.168.33.10/'
+    click_link '個室のあるお店'
+    select '東京', from: 'theme', visible: false
+    sleep 3
+    click_link '少人数で個室'
   end
 end
